729f6a4687e7ced246ac2c0999eca2520c8b6b36
Marcelo Bohn <marcelo.bohn@gmail.com>
2015-09-23 22:15:41 -0300

    insert callback function started server

--- cap02/hello_server.js
+++ cap02/hello_server.js
@@ -4,4 +4,6 @@ var server = http.createServer(function(request, response){
   response.write("<h1>Hello World!</h1>");
   response.end();
 });
-server.listen(3000);
+server.listen(3000, function(){ 
+	console.log('server running');
+});

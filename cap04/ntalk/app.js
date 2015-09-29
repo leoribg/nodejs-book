b93ac9399d0785526c149927796d1ffcca3271a6
Marcelo Bohn <marcelo.bohn@gmail.com>
2015-09-28 22:44:19 -0300

    defined start route

--- cap04/ntalk/app.js
+++ cap04/ntalk/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/home');
+// var users = require('./routes/users');
 
 var app = express();
 

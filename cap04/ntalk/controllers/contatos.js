7de6c776ac999cbfb898a74618e1e6bcbfe6163a
Marcelo Bohn <marcelo.bohn@gmail.com>
2015-09-29 21:07:03 -0300

    create controller contatos

---
+++ cap04/ntalk/controllers/contatos.js
@@ -0,0 +1,1 @@
+module.exports = function(app) {  var ContatoController = {    index: function(req, res) {      var usuario = req.session.usuario;      var params = {usuario: usuario};      res.render('contatos/index', params);    }}  return ContatoController;};
---
+++ cap04/ntalk/routes/contatos.js
@@ -0,0 +1,1 @@
+module.exports = function(app) {  var contatos = app.controllers.contatos;  app.get('/contatos', contatos.index);};
